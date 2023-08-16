4dafb7cf299f71859b897899324ce3bb19abff2a
Allan2103 <2020003442@ifam.edu.br>
2023-08-16 17:14:25 -0400

    feat: greeting controller and greeting

---
+++ src/main/java/edu/ifam/dra/greeting.java
@@ -0,0 +1,30 @@
+package edu.ifam.dra;
+
+public class greeting {
+	
+	private long id;
+	private String name;
+	
+	//constructor
+	public greeting(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	//methods
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
