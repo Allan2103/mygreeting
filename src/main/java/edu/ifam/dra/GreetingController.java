e42a0fdc3d85b9ddc3c1ca806f6675124f527dd6
Allan2103 <2020003442@ifam.edu.br>
2023-08-21 16:56:43 -0400

    ArrayList

--- src/main/java/edu/ifam/dra/GreetingController.java
+++ src/main/java/edu/ifam/dra/GreetingController.java
@@ -1,5 +1,8 @@
 package edu.ifam.dra;
 
+
+
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +15,17 @@ public class GreetingController {
 	
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	private ArrayList<Greeting> greetings = new ArrayList<Greeting>();
 	
 	@GetMapping("/Greeting")
 	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+		greetings.add(new Greeting(counter.incrementAndGet(), String.format(template, name)));
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@GetMapping("/Greetings")
+	public ArrayList<Greeting> greetings() {
+		return greetings;
 	}
 }
 
